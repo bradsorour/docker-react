3568ff57d6be329abf6c8217ad254dc03f41a204
Brad Sorour <brad.sorour@gmail.com>
2019-11-03 12:46:27 +0000

    small change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there dude!
+          Hi there Brad!
         </p>
         <a
           className="App-link"
